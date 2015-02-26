e050216ae57a83f4a0e1ed3ca9ed8af4144f936a
Yukihiro Moriyama <m.y.like.music.10@gmail.com>
2015-02-26 15:36:09 +0900

    Initial Commit

---
+++ ClapBeat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ClapBeat
+//
+//  Created by yukihiro moriyama on 2015/02/26.
+//  Copyright (c) 2015年 YukihiroMoriyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
